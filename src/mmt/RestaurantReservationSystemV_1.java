0b5b369550d675a57923a2ec664ba6d1ce970295
Muhammad Mahad Tariq <mmahad.bese14seecs@nust.edu.pk>
2017-03-15 16:48:08 +0500

    Add features

--- src/mmt/RestaurantReservationSystemV_1.java
+++ src/mmt/RestaurantReservationSystemV_1.java
@@ -5,6 +5,10 @@
  */
 package mmt;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author mmahad.bese14seecs
@@ -15,8 +19,26 @@ public class RestaurantReservationSystemV_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        System.out.println("Welcome to restaurant reservation system");
+        
+        System.out.println("You have to login to the system before you continue");
+        
+        System.out.println("");
+        
+        System.out.println("");
+        
+        
+        
         MysqlCon c1 = new MysqlCon();
+        
+        
+        
+        try {
+            c1.runSql("Select * from reservation where C_ID = " + "1");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(RestaurantReservationSystemV_1.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
